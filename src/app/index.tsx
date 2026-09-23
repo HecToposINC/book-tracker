6ca4357cb7a6eed65b19b8d745c22d7a5f16ca19
Héctor Eduardo Arévalo Hernández <94281654+HecToposINC@users.noreply.github.com>
2026-09-23 15:29:54 -0700

    Started the structure

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,60 +1,149 @@
 import * as Device from 'expo-device';
-import { Platform, StyleSheet } from 'react-native';
-import { SafeAreaView } from 'react-native-safe-area-context';
-
+import { useState } from 'react';
+import {
+  Button,
+  Modal,
+  Platform,
+  StyleSheet,
+  TextInput,
+} from 'react-native';import { SafeAreaView } from 'react-native-safe-area-context';
+import { Picker } from '@react-native-picker/picker'
 import { AnimatedIcon } from '@/components/animated-icon';
 import { HintRow } from '@/components/hint-row';
 import { ThemedText } from '@/components/themed-text';
+
 import { ThemedView } from '@/components/themed-view';
 import { WebBadge } from '@/components/web-badge';
 import { BottomTabInset, MaxContentWidth, Spacing } from '@/constants/theme';
-
-function getDevMenuHint() {
-  if (Platform.OS === 'web') {
-    return <ThemedText type="small">use browser devtools</ThemedText>;
-  }
-  if (Device.isDevice) {
-    return (
-      <ThemedText type="small">
-        shake device or press <ThemedText type="code">m</ThemedText> in terminal
-      </ThemedText>
-    );
-  }
-  const shortcut = Platform.OS === 'android' ? 'cmd+m (or ctrl+m)' : 'cmd+d';
-  return (
-    <ThemedText type="small">
-      press <ThemedText type="code">{shortcut}</ThemedText>
-    </ThemedText>
-  );
+enum Status {
+  owned = 'owned',
+  reading = 'reading',
+  read = 'read',
+  wishlist = 'wishlist',
 }
+  type Book = {
+    id: string;
+    title: string;
+    isbn: string;
+    status: Status
+  }
 
 export default function HomeScreen() {
+
+  const [isFormVisible, setIsFormVisible] = useState(false);
+const [title, setTitle] = useState('');
+const [isbn, setIsbn] = useState('');
+const[status, setStatus] = useState<Status>(Status.owned);
+const [books, setBooks]= useState<Book[]>([
+  {
+    id: '1',
+    title: 'この素晴らし世界に祝福を',
+    isbn: '1234',
+    status: Status.read
+  },  {
+    id: '2',
+    title: '魔女の度々',
+    isbn: '1236534',
+        status: Status.read
+
+  },  {
+    id: '3',
+    title: 'Aresen Lupin: La aguja hueca',
+    isbn: '1232145124',
+        status: Status.owned
+
+  }
+]);
   return (
     <ThemedView style={styles.container}>
       <SafeAreaView style={styles.safeArea}>
         <ThemedView style={styles.heroSection}>
           <AnimatedIcon />
           <ThemedText type="title" style={styles.title}>
-            Welcome to&nbsp;Expo
+            My libray
           </ThemedText>
         </ThemedView>
+        {
+          books.map((book)=> (
+            <HintRow
+            key={book.id}
+            title={book.title.trim()}
+            hint={<ThemedText type="code">{book.status+' '+book.isbn}</ThemedText>}
+            />
+          ))
+        }
+                  <Button
+  onPress={()=> setIsFormVisible(true)}
+  title="Add book"
+  color="#33CBDC"
+  accessibilityLabel="Add book"
+/>
+<Modal
+  visible={isFormVisible}
+  animationType="slide"
+  presentationStyle="pageSheet"
+  onRequestClose={() => setIsFormVisible(false)}
+>
+  <SafeAreaView style={styles.formContainer}>
+    <ThemedText type="title">Añadir libro</ThemedText>
 
-        <ThemedText type="code" style={styles.code}>
-          get started
-        </ThemedText>
+    <TextInput
+      style={styles.input}
+      placeholder="Título"
+      value={title}
+      onChangeText={setTitle}
+    />
 
-        <ThemedView type="backgroundElement" style={styles.stepContainer}>
-          <HintRow
-            title="Try editing"
-            hint={<ThemedText type="code">src/app/index.tsx</ThemedText>}
-          />
-          <HintRow title="Dev tools" hint={getDevMenuHint()} />
-          <HintRow
-            title="Fresh start"
-            hint={<ThemedText type="code">npm run reset-project</ThemedText>}
-          />
-        </ThemedView>
+    <TextInput
+      style={styles.input}
+      placeholder="ISBN"
+      value={isbn}
+      onChangeText={setIsbn}
+      keyboardType="numeric"
+    />
+    <Picker
+    selectedValue={status}
+ onValueChange={(itemValue) =>
+    setStatus(itemValue)}>
+      <Picker.Item label="Comprado" value={Status.owned} />
+<Picker.Item label="Leyendo" value={Status.reading} />
+<Picker.Item label="Leído" value={Status.read} />
+<Picker.Item label="Lista de deseos" value={Status.wishlist} />
+      </Picker>  
 
+    
+
+    <Button
+      title="Cancelar"
+      onPress={() =>{ setIsFormVisible(false);
+        setTitle('');
+        setIsbn('');
+        setStatus(Status.owned);
+      }}
+    />
+
+    <Button
+      title="Guardar"
+      onPress={() => {
+        if(title.trim().length > 0 && isbn.trim().length > 0){
+
+        
+        const newBook: Book = {
+  id: Date.now().toString(),
+  title: title.trim(),
+  isbn: isbn.trim(),
+      status
+
+};
+setBooks([...books, newBook]);
+        setIsFormVisible(false);
+        setTitle('');
+        setIsbn('');
+        setStatus(Status.owned);
+      }}}
+    />
+  </SafeAreaView>
+</Modal>
         {Platform.OS === 'web' && <WebBadge />}
       </SafeAreaView>
     </ThemedView>
@@ -95,4 +184,16 @@ const styles = StyleSheet.create({
     paddingVertical: Spacing.four,
     borderRadius: Spacing.four,
   },
+  formContainer: {
+  flex: 1,
+  padding: Spacing.four,
+  gap: Spacing.three,
+},
+input: {
+  borderWidth: 1,
+  borderColor: '#999',
+  borderRadius: 8,
+  padding: 12,
+  fontSize: 16,
+},
 });
